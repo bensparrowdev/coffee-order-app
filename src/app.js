35b9b0e3ec6753d3c0bbad487e7482a0906d9491
Ben Sparrow <bensparrowdev@gmail.com>
2021-03-24 10:33:57 +0000

    added functionality but errors

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
 const { require } = require('yargs');
 const yargs = require('yargs');
-const { addNote } = require('../utils/orders')
+const { addOrder, listOrders, removeOrder } = require('../utils/orders')
 
 const command = process.argv[2];
 
 
 if (command == "list") {
-    console.log("All orders: ")
+    console.log("All orders: ");
+    listOrders();
 }
 else if (command == "add") {
-    console.log("Adding to order...")
-    addNote(yargs.argv.order)
+    console.log("Adding to order...");
+    addOrder(yargs.argv.order);
 }
 else if (command == "remove") {
-    console.log("removing order item...")
+    console.log("removing order item...");
+    removeOrder(yargs.argv.order)
 }
--- utils/orders.js
+++ utils/orders.js
@@ -1,9 +1,53 @@
+const fs = require("fs");
+const { notStrictEqual } = require("node:assert");
+const { allowedNodeEnvironmentFlags } = require("node:process");
 
 
-const addOrder = (myOrder) => {
-    console.log(myOrder)
+const loadOrders = () => {
+    try {
+        const dataBuffer = fs.writeFileSync("src/orders.json");
+        const ordersJson = dataBuffer.toString();
+        return JSON.parse(ordersJson);
+    }   
+    catch (error) {
+        return []
+    }
 }
 
+
+const saveOrders = allOrders => {
+    const ordersJson = JSON.stringify(allOrders);
+    fs.writeFileSync("src/orders.json", ordersJson);
+}
+
+const addOrder = (myOrder) => {
+    const allOrders = loadOrders()
+    allOrders.push({reminder: myOrder})
+
+    saveOrders(allOrders);
+}
+
+const listOrders = () => {
+    const allOrders = loadOrders();
+
+    allOrders.map(order => {
+        console.log(order);
+    });
+}
+
+const removeOrder = orderToDelete => {
+    const allOrders = loadOrders();
+
+    const ordersToKeep = allOrders.filter(order => {
+        return order.reminder != orderToDelete;
+    });
+
+    saveOrders(ordersToKeep);
+}
+
+
 module.exports = {
-    addOrder
+    addOrder,
+    listOrders,
+    removeOrder
 }
